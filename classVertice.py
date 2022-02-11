295067b8286b03d5ee07e6082735efc06e9d4dcd
a-lvaro <38051336+a-lvaro@users.noreply.github.com>
2022-02-11 13:51:31 -0300

    Add files via upload

---
+++ classVertice.py
@@ -0,0 +1,16 @@
+from cmath import inf
+
+
+class Vertice:
+    def __init__(self, vertice) -> None:
+        self.profundidade = inf
+        self.vertice = vertice
+        self.aresta = []
+        self.pai = inf
+
+    def addAresta(self, u: int, v: int):
+        if u == self.vertice:
+            self.aresta.append(v)
+
+        else:
+            self.aresta.append(u)
