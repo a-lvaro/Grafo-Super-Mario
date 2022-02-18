ceaf03e921b314d2944a80b00f130117f909d9c4
a-lvaro <aaflneto@gmail.com>
2022-02-18 19:31:10 -0300

    big updates

--- bfs.py
+++ bfs.py
@@ -1,2 +1,18 @@
+from cmath import inf
+
+
 class Bfs:
-    pass
+    def __init__(self, grafo: object, verticeInicio: int) -> None:
+        aux = []
+
+        grafo.setProfundidade(verticeInicio, 0)
+        aux.append(verticeInicio)
+
+        while aux:
+            u = aux.pop(0)
+
+            for vertice in grafo.getAdjacentes(u):
+                if grafo.getProfundidade(vertice) == inf:
+                    aux.append(vertice)
+                    grafo.setProfundidade(
+                        vertice, grafo.getProfundidade(u) + 1)
