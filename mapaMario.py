75b0685447598311514cf1e5f3a10914155d0720
a-lvaro <aaflneto@gmail.com>
2022-03-17 23:39:37 -0300

    fechar arquivo aberto

--- mapaMario.py
+++ mapaMario.py
@@ -4,8 +4,9 @@ from numpy import loadtxt, int8
 class MapaMario:
     # tirar as arestas da matriz e colocar em lista
     def __init__(self, vertices: list):
-        file = open('matriz.csv')
-        matrizMapa = loadtxt(file, delimiter=',', dtype=int8)
+
+        with open('matriz.csv') as file:
+            matrizMapa = loadtxt(file, delimiter=',', dtype=int8)
 
         for i in range(96):
             for j in range(96):
